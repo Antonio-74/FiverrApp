5309a5d369580d44893a2e35e2459c7456a9cd3c
Antonio-74 <antoniolopezvalles604@gmail.com>
2023-07-08 19:52:55 -0700

    React Product Slider

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Home.scss';
-import { cards } from '../../data';
+import { cards, projects } from '../../data';
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Slide from '../../components/slide/Slide';
 import CatCard from '../../components/catCard/CatCard';
+import ProjectCard from '../../components/projectCard/ProjectCard';
 
 function Home() {
   return (
@@ -91,6 +92,11 @@ function Home() {
           </div>
         </div>
       </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map(card => (
+          <ProjectCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   )
 }
